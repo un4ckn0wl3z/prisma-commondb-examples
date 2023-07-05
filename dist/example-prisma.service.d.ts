fdadd09edc7f9a896db7d9ea26bee61788aa62d0
un4ckn0wl3z <admin@unknownclub.net>
2023-07-05 17:21:01 +0700

    feat: update

---
+++ dist/example-prisma.service.d.ts
@@ -0,0 +1,8 @@
+import { INestApplication, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from './prisma/commondb_examples';
+export declare class ExamplePrismaService extends PrismaClient implements OnModuleInit {
+    private injectTsFieldMiddleware;
+    onModuleInit(): Promise<void>;
+    enableShutdownHooks(app: INestApplication): Promise<void>;
+}
+//# sourceMappingURL=example-prisma.service.d.ts.map
---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+export * from './example-prisma.module';
+export * from './example-prisma.service';
+//# sourceMappingURL=index.d.ts.map
