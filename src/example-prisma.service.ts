fc6af01aac892342b11399eeb8535c961b73ee2a
un4ckn0wl3z <admin@unknownclub.net>
2023-07-05 17:16:31 +0700

    feat: update

--- src/example-prisma.service.ts
+++ src/example-prisma.service.ts
@@ -1,5 +1,5 @@
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/commondb_examples';
+import { PrismaClient } from './prisma/commondb_examples';
 import { ObjectId } from 'bson';
 
 @Injectable()
